c425db16fc1262c512884cf1f42f3e82b89b24b1
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-14 23:19:23 -0600

    adding style

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ function initMap() {
 var map;
 map = new google.maps.Map(document.getElementById('map'), {
 center: {lat: 41.8789, lng: -87.6359},
-zoom: 8
+zoom: 8,
+styles: [
+            {elementType: 'geometry', stylers: [{color: 'Purple'}]}
  });
 }
   
