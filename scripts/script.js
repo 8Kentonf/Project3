9022ef206c7a1b023aa265ce7973e8d41ca139ef
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-14 23:22:02 -0600

    adding road style

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,14 @@ function initMap() {
 var map;
 map = new google.maps.Map(document.getElementById('map'), {
 center: {lat: 41.8789, lng: -87.6359},
-zoom: 8,
+zoom: 15,
 styles: [
-            {elementType: 'geometry', stylers: [{color: 'Purple'}]}
+            {elementType: 'geometry', stylers: [{color: '#6A5ACD'}]},
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#FFFF00'}]
+            },
  });
 }
   
