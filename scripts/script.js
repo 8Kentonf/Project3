c0726c2e508de69ba5070d0986edaf622e82dd15
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-14 18:25:42 -0600

    testing map lat lng

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
 function initMap() {
   var sears = {lat: 41.8789 , lng: 87.6359};
-  var map = new google.maps.Map(document.getElementById('map'), {zoom: 5, center: sears});
+  var map = new google.maps.Map(document.getElementById('map'),{
+  zoom: 5, 
+  center: {lat: 41.8789 , lng: 87.6359};
+  });
