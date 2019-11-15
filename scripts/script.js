1b5fd1fc67ac3acdf379d4890533a5d4c87ed96a
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-14 18:26:28 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 function initMap() {
-  var sears = {lat: 41.8789 , lng: 87.6359};
   var map = new google.maps.Map(document.getElementById('map'),{
   zoom: 5, 
   center: {lat: 41.8789 , lng: 87.6359};
   });
+  
+}
