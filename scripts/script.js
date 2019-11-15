2ff085da4b57d601321ae71804886246741d9d9e
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-14 22:52:29 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 function initMap() {
 var map;
 map = new google.maps.Map(document.getElementById('map'), {
-center: {lat: 41.8789, lng: 87.6359},
+center: {lat: 41.8789, lng: -87.6359},
 zoom: 8
  });
 }
