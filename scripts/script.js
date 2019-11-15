86f57848d71507d826ccf259c9ace4921e059d02
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-14 23:31:35 -0600

    adding transit station style

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,8 @@ map = new google.maps.Map(document.getElementById('map'), {
 center: {lat: 41.8789, lng: -87.6359},
 zoom: 15,
 styles: [
-            {elementType: 'geometry', stylers: [{color: '#6A5ACD'}]},
+            {elementType: 'geometry', 
+            stylers: [{color: '#6A5ACD'}]},
             {
               featureType: 'road',
               elementType: 'geometry',
@@ -19,6 +20,11 @@ styles: [
               featureType: 'water',
               elementType: 'geometry',
               stylers: [{color: '#FFE4E1'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#800000'}]
             },
             ]
 });
