404b37fe2f10fc00f3fa9ebbb1993221e97e11be
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-14 23:48:23 -0600

    formatting

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,49 @@
 function initMap() {
 
-var sears = {lat: 41.8789, lng: -87.6359};
- 
-var marker = new google.maps.Marker({position: sears, map: map});
+	var sears = {
+		lat: 41.8789,
+		lng: -87.6359
+	};
 
-var map;
-map = new google.maps.Map(document.getElementById('map'), {
-center: {lat: 41.8789, lng: -87.6359},
-zoom: 15,
-styles: [
-            {elementType: 'geometry', 
-            stylers: [{color: '#6A5ACD'}]},
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#FFFF00'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#FFE4E1'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#800000'}]
-            },
-            ]
-});
+	var marker = new google.maps.Marker({
+		position: sears,
+		map: map
+	});
+
+	var map;
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: 41.8789,
+			lng: -87.6359
+		},
+		zoom: 15,
+		styles: [{
+				elementType: 'geometry',
+				stylers: [{
+					color: '#6A5ACD'
+				}]
+			},
+			{
+				featureType: 'road',
+				elementType: 'geometry',
+				stylers: [{
+					color: '#FFFF00'
+				}]
+			},
+			{
+				featureType: 'water',
+				elementType: 'geometry',
+				stylers: [{
+					color: '#FFE4E1'
+				}]
+			},
+			{
+				featureType: 'transit.station',
+				elementType: 'labels.text.fill',
+				stylers: [{
+					color: '#800000'
+				}]
+			},
+		]
+	});
 }
-  
